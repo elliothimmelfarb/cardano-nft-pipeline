7204590d04e231ed5872c52497e073f31584b490
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-11-13 19:01:19 -0800

    we have contact

--- src/cardano.ts
+++ src/cardano.ts
@@ -3,13 +3,13 @@ import { Blockfrost, Lucid } from 'https://deno.land/x/lucid@0.7.6/mod.ts'
 export const cardano = async () => {
   const lucid = await Lucid.new(
     new Blockfrost(
-      'https://cardano-preview.blockfrost.io/api/v0',
+      'https://cardano-mainnet.blockfrost.io/api/v0',
       'mainnetBm6PwgxKgpgISoC1dr9OZSTU1orxZjCw',
     ),
     'Mainnet',
   )
 
-  const slot = await lucid.currentSlot()
+  const slot = lucid.currentSlot()
 
   console.log({ slot })
 }
