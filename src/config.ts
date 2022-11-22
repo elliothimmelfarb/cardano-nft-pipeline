cc7ca57b6bc9873835bf316f85d5f1fab4985ca0
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-11-22 13:19:36 -0800

    automate metadata

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  inputFolder: '',
+  collectionDescription:
+    'Here is a beautiful description for this Testing Collection',
+  collectionName: 'Testing Collection',
+}
--- src/mint/mintNFTs.ts
+++ src/mint/mintNFTs.ts
@@ -1,57 +1,69 @@
 import { Unit, utf8ToHex } from 'https://deno.land/x/lucid@0.7.6/mod.ts'
-import { getWallet } from '../wallet/getWallet.ts'
 import { lucid } from '../lucid.ts'
 import { mintingPolicy } from './mintingPolicy.ts'
+import { config } from '../config.ts'
 
-export const mintNFTs = async (name: string) => {
-  const wallet = getWallet()
-
-  if (!wallet) return console.error('no wallet found')
-
+export const mintNFTs = async () => {
   const policy = await mintingPolicy()
 
-  const metadata = {
-    [policy.id]: {
-      [name]: {
-        name: name,
-        description: 'An NFT minted by Lucid',
-        mediaType: 'image/svg',
-        files: [
-          {
-            mediaType: 'image/svg',
-            name: name,
-            src: 'https://developers.cardano.org/img/cardano-white.svg',
-          },
-        ],
-        image: 'https://developers.cardano.org/img/cardano-white.svg',
-      },
-      [name + '2']: {
-        name: name + '2',
-        description: 'An NFT minted by Lucid',
-        mediaType: 'image/svg',
-        files: [
-          {
-            mediaType: 'image/svg',
-            name: name + '2',
-            src: 'https://developers.cardano.org/img/cardano-white.svg',
-          },
-        ],
-        image: 'https://developers.cardano.org/img/cardano-white.svg',
-      },
+  const inputAssets = [
+    {
+      mediaType: 'image/png',
+      image: 'QmciQW1JFjzjxvqMKuQ2M2ohuXENf3sEG3HsyGAqQTRGNZ',
+      meta1: 'ooga',
+      meta2: 'booga',
     },
+    {
+      mediaType: 'image/png',
+      image: 'QmWdrHV497Twq24rSUMdDRgh5F8Ag1QQqAUjS9gr4e4vgY',
+      meta1: 'tray',
+      meta2: 'tro',
+    },
+  ]
+
+  const metadataAssets = inputAssets
+    .map((asset, index) => {
+      const name = `${config.collectionName} #${index + 1}`
+      return {
+        ...asset,
+        name,
+        description: config.collectionDescription,
+        files: [
+          {
+            mediaType: asset.mediaType,
+            name,
+            src: `ipfs://${asset.image}`,
+          },
+        ],
+      }
+    })
+    .reduce((out, asset) => {
+      return {
+        ...out,
+        [asset.name]: asset,
+      }
+    }, {})
+
+  const metadata = {
+    [policy.id]: metadataAssets,
     version: '1.0',
   }
 
-  console.log('metadata:', JSON.stringify(metadata))
+  const assets = Object.keys(metadataAssets)
+    .map((name): Unit => policy.id + utf8ToHex(name))
+    .reduce(
+      (out, name) => ({
+        ...out,
+        [name]: 1n,
+      }),
+      {},
+    )
 
-  // const unit: Unit = policyId + utf8ToHex(name)
+  console.log('metadata:', JSON.stringify({ 721: metadata }))
 
   const tx = await lucid
     .newTx()
-    .mintAssets({
-      [policy.id + utf8ToHex(name)]: 1n,
-      [policy.id + utf8ToHex(name + 2)]: 1n,
-    })
+    .mintAssets(assets)
     .attachMetadata(721, metadata)
     .validTo(Date.now() + 100000)
     .attachMintingPolicy(policy.script)
