4aa1c172c0f991670673e70c9296c9ff6ca81ffa
elliothimmelfarb <elliothimmelfarb@gmail.com>
2022-12-06 20:20:37 -0800

    reading image metadata

--- src/metadata/createAssets.ts
+++ src/metadata/createAssets.ts
@@ -1,11 +1,20 @@
+import exifr from 'npm:exifr'
 import { config } from '../config.ts'
 
 export const createAssets = async () => {
   const dir = await Deno.readDirSync(Deno.cwd() + config.imagesPath)
 
   for (const dirEntry of dir) {
-    if (!dirEntry.name.includes('.png')) continue
+    if (dirEntry.name === '.gitkeep') continue
 
-    console.log(dirEntry.name)
+    const path = Deno.cwd() + config.imagesPath + '/' + dirEntry.name
+
+    console.log('path:', path)
+
+    const buffer = await Deno.readFile(path)
+
+    const data = await exifr.parse(buffer)
+
+    console.log(JSON.parse(data['sd-metadata']))
   }
 }
